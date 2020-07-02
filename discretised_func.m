076bf31abb1dbf70cfcabb77b2f7d38f1154f30b
tamaratambyah <49504383+tamaratambyah@users.noreply.github.com>
2020-07-02 14:50:33 +1000

    Update discretised_func.m

--- discretised_func.m
+++ discretised_func.m
@@ -1,6 +1,6 @@
 function g = discretised_func(q,q_old,k,a,nodes,dt,s_old,s)
 % FUNCTION DISCRETISED_FUNC 
-% returns the discretised verision of Equation (2.32)
+% returns the discretised verision of Equation (2.31)
 
 global dz z eta
 g = zeros(nodes,1); % initialise output
@@ -8,10 +8,10 @@ g = zeros(nodes,1); % initialise output
 r = dt/(eta*dz^2*s_old^2); % constant 
 ds_dt = (s-s_old)/dt; % change in s
 
-% boundary condition at x = 0, Equation (2.34)
+% boundary condition at x = 0, Equation (2.33)
 g(1) = k(2)*(1/q(2)-a(2)) - k(1)*(1/q(1)-a(1)); 
 
-% internal nodes, Equation (2.32)
+% internal nodes, Equation (2.31)
 for i = 2:length(z)-1
     C = z(i)*dt/(2*s_old*dz);
     g(i) = - q(i) + q_old(i) ...
@@ -19,7 +19,7 @@ for i = 2:length(z)-1
         + C*ds_dt*(q(i+1)-q(i-1)) ;
 end
 
-% boundary condition at x = s(t), Equation (2.35)
+% boundary condition at x = s(t), Equation (2.34)
 i = nodes;
 g(i) = 1/(2*q(i)*s_old*dz)*(k(i)*(1/q(i)-a(i)) - k(i-1)*(1/q(i-1)-a(i-1))) ...
     + k(i)*(1/q(i)-a(i)) ...
